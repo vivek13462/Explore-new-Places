03b4348183ad0375562620b29710c9ffc6b6ffec
vivek13462 <vivek.13462@gmail.com>
2016-12-04 23:17:40 -0800

    changes in db

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
 module.exports = {
-    'url' : 'mongodb://admin:admin@ds015942.mlab.com:15942/foodnearme' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+     // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://vivek:123@ds119738.mlab.com:19738/explore_places'
+    //'url' : 'mongodb://admin:admin@ds015942.mlab.com:15942/foodnearme'
 };
